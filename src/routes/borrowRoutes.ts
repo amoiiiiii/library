7789ec3af3c38ac5ec7d93c2d8d556933209522a
amoi <156885501+amoiiiiii@users.noreply.github.com>
2024-09-18 16:07:31 +0700

    Delete src directory

--- src/routes/borrowRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { borrowBook, returnBook } from '../controllers/borrowController';
-
-const router = Router();
-
-router.post('/', borrowBook);
-router.put('/:id/return', returnBook);
-
-export default router;
--- src/types/custom.d.ts
+++
@@ -1,9 +0,0 @@
-import { User } from '@prisma/client';  // Pastikan path dan tipe sesuai dengan model di Prisma
-
-declare global {
-  namespace Express {
-    interface Request {
-      user?: User;  // Definisikan tipe user di sini
-    }
-  }
-}
